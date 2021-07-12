c6fc8b940f3037499e28b79e52206f235c01fd79
Joshua Hulbert <43861345+jhulbert-va@users.noreply.github.com>
2021-07-12 13:36:08 -0400

    Add configurable timeouts to MPI client (#14)

--- master-patient-index-client/src/main/java/gov/va/api/lighthouse/mpi/SoapMasterPatientIndexClient.java
+++ master-patient-index-client/src/main/java/gov/va/api/lighthouse/mpi/SoapMasterPatientIndexClient.java
@@ -136,6 +136,18 @@ public class SoapMasterPatientIndexClient implements MasterPatientIndexClient {
         bp.getRequestContext()
             .put(com.sun.xml.ws.developer.JAXWSProperties.SSL_SOCKET_FACTORY, socketFactory);
       }
+      if (config().getConnectionTimeout() != null) {
+        bp.getRequestContext()
+            .put(
+                com.sun.xml.ws.developer.JAXWSProperties.CONNECT_TIMEOUT,
+                config().getConnectionTimeout());
+      }
+      if (config().getReadTimeout() != null) {
+        bp.getRequestContext()
+            .put(
+                com.sun.xml.ws.developer.JAXWSProperties.REQUEST_TIMEOUT,
+                config().getReadTimeout());
+      }
       bp.getRequestContext().put(BindingProvider.ENDPOINT_ADDRESS_PROPERTY, config.getUrl());
       return port;
     } catch (InaccessibleWSDLException e) {
